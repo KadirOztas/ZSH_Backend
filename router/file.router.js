6987de1d97477cbe3b78e782995bcee8f86082e9
KadirOztas <m.abdulkadiroztas@gmail.com>
2024-05-05 16:22:17 +0200

    Added upload routes for each kanton

--- router/file.router.js
+++ router/file.router.js
@@ -1,22 +1,31 @@
-
 import express from "express";
 import { upload } from "../service/file.service.js";
 import { verifyToken } from "../utility/auth.utility.js";
 
 const router = express.Router();
 
-router.post("/upload", verifyToken, upload.single("file"), (req, res) => {
-	try {
-		if (!req.file) {
-			return res.status(400).json({ message: "No file uploaded" });
+
+const kantons = ["uri", "zug", "luzern", "nidwalden", "obwalden", "schwyz"];
+
+kantons.forEach((kanton) => {
+	router.post(
+		`/upload/${kanton}`,
+		verifyToken,
+		upload.single("file"),
+		(req, res) => {
+			try {
+				if (!req.file) {
+					return res.status(400).json({ message: "No file uploaded" });
+				}
+				return res
+					.status(201)
+					.json({ message: "File uploaded successfully", file: req.file });
+			} catch (error) {
+				console.error("File upload error: ", error);
+				return res.status(500).json({ message: "Internal Server Error" });
+			}
 		}
-		return res
-			.status(201)
-			.json({ message: "File uploaded successfully", file: req.file });
-	} catch (error) {
-		console.error("File upload error: ", error);
-		return res.status(500).json({ message: "Internal Server Error" });
-	}
+	);
 });
 
-export  {router};
+export { router };
