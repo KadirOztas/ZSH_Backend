82667e0b19f17f4e5afe49ec8fa90b91dfa088e4
KadirOztas <m.abdulkadiroztas@gmail.com>
2024-05-05 22:20:51 +0200

    Solved saving files issue

--- router/file.router.js
+++ router/file.router.js
@@ -5,6 +5,7 @@ import fs from "fs";
 import { fileURLToPath } from "url";
 import { verifyToken } from "../utility/auth.utility.js";
 import { upload } from "../service/file.service.js";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,41 +13,33 @@ const router = express.Router();
 
 const kantons = ["uri", "zug", "luzern", "nidwalden", "obwalden", "schwyz"];
 
-
 kantons.forEach((kanton) => {
 	router.post(
-		`/upload/${kanton}`,
+		"/upload/:kanton",
 		verifyToken,
 		upload.single("file"),
-		(req, res, next) => {
+		(req, res) => {
+			console.log(
+				`File upload request received for kanton: ${req.params.kanton}`
+			);
 			if (!req.file) {
+				console.log("No file was uploaded.");
 				return res.status(400).json({ message: "No files were uploaded." });
 			}
 			const file = req.file;
-			const uploadPath = path.join(
-				__dirname,
-				"../uploads",
-				kanton,
-				file.originalname
-			);
-			fs.promises
-				.copyFile(file.path, uploadPath)
-				.then(() => {
-					next();
-				})
-				.catch((err) => {
-					console.error("File upload error: ", err);
-					return res.status(500).json({ message: "File upload failed." });
-				});
-		},
-		(req, res) => {
-			res
-				.status(201)
-				.json({ message: "File uploaded successfully", file: req.file.path });
+			console.log(`File saved to: ${file.path}`);
+			res.status(201).json({
+				message: "File uploaded successfully",
+				path: file.path,
+			});
 		}
 	);
+
 	router.get(`/upload/${kanton}`, (req, res) => {
 		const dirPath = path.join(__dirname, "../uploads", kanton);
+		console.log(`Processing file upload at ${new Date().toISOString()}`);
+		console.log(`GET request URL: ${req.originalUrl}`);
+
 		fs.readdir(dirPath, (err, files) => {
 			if (err) {
 				console.error(`Error reading the directory for ${kanton}:`, err);
