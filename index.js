cbdbaaef847b66dd5786e06958af4bf66094d1b3
KadirOztas <m.abdulkadiroztas@gmail.com>
2024-05-19 23:26:50 +0200

    Added cors origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://zsh-backend.onrender.com",
 		credentials: true,
 	})
 );
